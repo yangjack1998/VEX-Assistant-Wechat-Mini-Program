61e79bde3a9f4e324dabef6249bfcf9404b022a9
Jack Yang <yyang17@wpi.edu>
2020-04-26 18:09:45 -0400

    Setup ruletest page

---
+++ ruleQuiz/ruleQuiz.js
@@ -0,0 +1,76 @@
+// ruleQuiz/ruleQuiz.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+      question:"测试"
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  choose(event) {
+    this.setData({
+      question:"测试1"
+    })
+    
+  }
+
+
+
+})
