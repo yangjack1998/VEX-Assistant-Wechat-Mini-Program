6a55ded12d50e2cd1ee7f7ac097f7783777c0ab1
Jack Yang <yyang17@wpi.edu>
2020-06-02 23:54:42 -0400

    Added timer for rule quiz, added more feature to ranking system Added global variable to store score and time. Now only the highest score will be recorded. If scores are same, the faster one will be recorded. Same rule apply to the ranking.

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -14,8 +14,27 @@ App({
         traceUser: true,
       })
     }
+    this.onGetOpenid()
+    this.globalData = {
 
-    this.globalData = {}
+    }
+    
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    let that = this
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        that.globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
   },
 
 
