905a76c53a326a88372e85fda5a14438efd54e14
Jack Yang <yyang17@wpi.edu>
2023-11-18 16:52:14 -0500

    Separete team digest to its own page

--- miniprogram/pages/subPages/search/search.js
+++ miniprogram/pages/subPages/search/search.js
@@ -6,16 +6,7 @@ Component({
     // 页面的初始数据
     data: {
         team_num: "",
-        team_num2: "",
-        team_name: '',
-        org: '',
-        robot_name: '',
-        city: '',
-        region: '',
-        grade: '',
         disable: true,
-        hide: false,
-        events: []
     },
 
     // 生命周期函数
@@ -24,7 +15,7 @@ Component({
     },
 
     methods: {
-        // 新增的独立方法，用于调用云函数
+        // 新增的独立方法，用于调用云函数获取基本队伍信息
         getTeamBaseInfo() {
             wx.cloud.callFunction({
                 config: { env: 'vex-assistant-1gwxsfv3ad6fda91' },
@@ -33,98 +24,54 @@ Component({
                     team_num: this.data.team_num,
                 },
             })
-                .then(res => {
-                    this.handleSearchTeamResult(res);
-                })
-                .catch(console.error)
-        },
-
-        getTeamSeasonDigest() {
-            wx.cloud.callFunction({
-                config: { env: 'vex-assistant-1gwxsfv3ad6fda91' },
-                name: 'searchSeasonDigest',
-                data: {
-                    team_num: this.data.team_num,
-                },
+            .then(res => {
+                this.handleSearchTeamResult(res);
             })
-                .then(res => {
-                    this.handleSearchDigestResult(res);
-                })
-                .catch(console.error)
-        },
-
-        // 处理云函数返回的结果
-        handleSearchDigestResult(res) {
-            const info = res.result;
-            console.log(info)
-            if (info.length < 1) {
-                wx.showToast({
-                    title: '查询的队伍编号不存在，请重新输入',
-                    icon: 'none',
-                    duration: 3000
-                })
-                this.setData({
-                    team_num: ''
-                })
-                return;
-            } else {
-                this.setData({
-                    events: info
-                });
-                console.log(this.data.events)
-            }
+            .catch(console.error)
         },
 
         // 处理云函数返回的结果
         handleSearchTeamResult(res) {
-            console.log(res) // 3
+            console.log(res);
             const info = res.result;
             if (info.length < 1) {
                 wx.showToast({
                     title: '查询的队伍编号不存在，请重新输入',
                     icon: 'none',
                     duration: 3000
-                })
+                });
                 this.setData({
                     team_num: ''
-                })
+                });
                 return;
             } else {
-                this.setData({
-                    hide: false,
-                    team_num2: info.number,
-                    team_name: info.team_name,
-                    robot_name: info.robot_name,
-                    org: info.organization,
-                    city: info.location.city,
-                    region: info.location.country,
-                    grade: info.grade,
-                })
+                // 跳转到新页面并传递队伍编号
+                wx.navigateTo({
+                    url: '/pages/teamDigest/teamDigest?team_num=' + this.data.team_num
+                });
             }
-            console.log(info);
         },
 
         // 提交表单的方法
         submit(event) {
             console.log("Search for team");
             this.getTeamBaseInfo();
-            this.getTeamSeasonDigest();
         },
 
         bindKeyInput: function (e) {
             this.setData({
                 team_num: e.detail.value
-            })
-            let input = this.data.team_num
+            });
+            let input = this.data.team_num;
 
             if (input.length > 0) {
                 this.setData({
                     disable: false
-                })
+                });
             } else {
                 this.setData({
                     disable: true
-                })
+                });
             }
         },
     }
--- miniprogram/pages/teamDigest/teamDigest.js
+++ miniprogram/pages/teamDigest/teamDigest.js
@@ -1,66 +1,70 @@
-// pages/teamDigest.js
+// teamDetails.js
+const app = getApp();
+const db = wx.cloud.database();
+const _ = db.command;
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        team_num: "",
+        events: [],
+        teamDetails: {},
+        // 可以根据需要添加更多的数据字段
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+    onLoad: function (options) {
+        // 获取从上一个页面传递过来的队伍编号
+        console.log(options.team_num); // 打印以确保数据传递正确
+        this.setData({
+            team_num: options.team_num
+        });
 
+        // 调用函数获取队伍详细信息
+        this.getTeamDetails();
+        this.getTeamSeasonDigest();
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
+    getTeamDetails() {
+        wx.cloud.callFunction({
+            config: { env: 'vex-assistant-1gwxsfv3ad6fda91' },
+            name: 'searchTeam',
+            data: {
+                team_num: this.data.team_num,
+            },
+        })
+        .then(res => {
+            this.setData({
+                teamDetails: res.result
+            });
+        })
+        .catch(console.error);
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
+    getTeamSeasonDigest() {
+        wx.cloud.callFunction({
+            config: { env: 'vex-assistant-1gwxsfv3ad6fda91' },
+            name: 'searchSeasonDigest',
+            data: {
+                team_num: this.data.team_num,
+            },
+        })
+        .then(res => {
+            console.log( this.data.team_num)
+            console.log(res.result)
+            this.setData({
+                events: res.result
+            });
+        })
+        .catch(console.error);
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
+    toggleEvent: function(e) {
+        const index = e.currentTarget.dataset.index;
+        const toggle = `events[${index}].expanded`;
+        this.setData({
+            [toggle]: !this.data.events[index].expanded
+        });
     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+    // 可以添加更多的方法来处理用户交互或数据处理
 
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
-})
+});
